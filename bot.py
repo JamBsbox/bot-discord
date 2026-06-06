26418ea6b9f2e410fef239f6719f0810d1f30be2
JamB <maximequeirosjambonbeurre@gmail.com>
2026-06-06 18:45:04 +0200

    premier commit propre

---
+++ bot.py
@@ -0,0 +1,21 @@
+import discord
+from discord.ext import commands
+import os
+
+intents = discord.Intents.default()
+intents.message_content = True
+bot = commands.Bot(command_prefix="!", intents=intents)
+
+@bot.event
+async def on_ready():
+    print(f"Connecté en tant que {bot.user}")
+
+@bot.command()
+async def bonjour(ctx):
+    await ctx.send(f"Bonjour {ctx.author.name} !")
+
+@bot.command()
+async def ping(ctx):
+    await ctx.send("Pong ! 🏓")
+
+bot.run(os.environ["MTUxMDM1NDYyMjQzNzY1NDc1MA.G7MelN.FbShvwItypT9wHqmiwOIGfPA182YaBelDIHGlM"])
